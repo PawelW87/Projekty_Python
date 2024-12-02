40d166a2fc55488dce4ab9092185cbe5eb8c1e55
Pawel87W <wojtynski.pawel87@gmail.com>
2024-12-02 20:23:35 +0100

    dodano klasę lakiery

--- file1.py
+++ file1.py
@@ -1,3 +1,22 @@
 """this is the first file in training repo"""
 ' this line is for "new-features" branch'
 'this line was created in gitgub in develop branch'
+
+class Lakiery:
+    def __init__(self, rodzaj: str, kolor: str):
+        self.rodzaj = rodzaj
+        self.kolor = kolor
+
+    def __str__(self):
+        print(f"lakier {self.rodzaj}, kolor {self.kolor}.")
+
+lak1 = Lakiery("nitroceluloza", "bezbarwny")
+lak2 = Lakiery('akrylowy', 'biały')
+lak3 = Lakiery('ftalowy', 'zielony')
+
+list = [lak1, lak2, lak3]
+
+for l in list:
+    l.__str__()
+
+
