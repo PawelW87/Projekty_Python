fed9d39395d12ee1b6beb03dd632e41b8708a769
Pawel87W <wojtynski.pawel87@gmail.com>
2024-12-08 20:42:34 +0100

    Usunięto zbędny kod, naprawiona literówka.

--- file1.py
+++ file1.py
@@ -2,23 +2,6 @@
 ' this line is for "new-features" branch'
 'this line was created in gitgub in develop branch'
 
-# class Lakiery:
-#     def __init__(self, rodzaj: str, kolor: str):
-#         self.rodzaj = rodzaj
-#         self.kolor = kolor
-
-#     def __str__(self):
-#         print(f"lakier {self.rodzaj}, kolor {self.kolor}.")
-
-# lak1 = Lakiery("nitroceluloza", "bezbarwny")
-# lak2 = Lakiery('akrylowy', 'biały')
-# lak3 = Lakiery('ftalowy', 'zielony')
-
-# list = [lak1, lak2, lak3]
-
-# for l in list:
-#     l.__str__()
-
 
 import csv 
 from dataclasses import dataclass
@@ -49,4 +32,4 @@ class Transactions:
         date_format = self.time
 
     def show_transaction(self):
-        print(f"{self.id} {self.operations} {self.date_format} {self.amount}{self.comment}")
+        print(f"{self.id} {self.operation} {self.date_format} {self.amount}{self.comment}")
