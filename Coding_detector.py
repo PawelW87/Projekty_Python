e6cb683bf0fed123df803f45e54c6c6478342854
Pawel87W <wojtynski.pawel87@gmail.com>
2024-12-16 11:42:16 +0100

    _-----_

--- Coding_detector.py
+++ Coding_detector.py
@@ -2,8 +2,8 @@
 
 import chardet
 
-with open(r'csv_files\Custom_.csv', 'rb') as file:
-    raw_data = file.read()  # Wczytaj pierwsze 10KB pliku
+with open(r'csv_files\Custom_2024-01.csv', 'rb') as file:
+    raw_data = file.read(10000)  # Reads first 10KB.
     result = chardet.detect(raw_data)
     encoding = result['encoding']
     print(f"Kodowanie wykryte przez chardet: {encoding}")
